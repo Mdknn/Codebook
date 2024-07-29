7fb9b8467cd6029604416e424ffc00d707ae3874
Mdknn <mdkaif123000@gmail.com>
2024-07-29 23:31:47 +0530

    first comment

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { ToastContainer } from 'react-toastify';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import { ToastContainer } from 'react-toastify';
+
 import 'react-toastify/dist/ReactToastify.css';
 
 import { FilterProvider, CartProvider } from "./context";
